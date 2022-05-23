41d223cc4b18261a97475433f22b728cf9dd3041
MOKXF <mokxf16@gmail.com>
2022-05-23 23:12:40 +0800

    feat: user can guess word

--- src/components/Letters.js
+++ src/components/Letters.js
@@ -5,28 +5,55 @@ const Letters = () => {
 	const alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	alphabetArray = alphabets.split("");
 
-	// REPLACE THIS WITH RANDOM WORD
+	// TODO: REPLACE THIS WITH RANDOM WORD
 	const [answer, setAnswer] = useState("hello");
 	const [guessed, setGuessed] = useState(new Set([]));
+	const [mistakes, setMistakes] = useState(0);
+
+	const handleClickLetter = (e) => {
+		const letter = e.target.value;
+		const newGuessed = guessed.add(letter);
+		setGuessed(new Set(newGuessed));
+		console.log(guessed);
+	};
+
+	const resetGame = () => {
+		// TODO: REPLACE THIS WITH RANDOM WORD
+		setAnswer("cat");
+		setGuessed(new Set([]));
+		setMistakes(0);
+	};
 
 	return (
 		<div className="text-center">
-			<div className="mb-4">
+			<h2 className="mb-5">
 				{answer
 					.split("")
-					.map((letter) => (guessed.has(letter) ? letter : " _ "))}
-			</div>
+					.map((letter) =>
+						guessed.has(letter.toUpperCase())
+							? ` ${letter.toUpperCase()} `
+							: " _ "
+					)}
+			</h2>
 			<div>
 				{alphabetArray.map((a) => (
 					<button
 						type="button"
-						class="btn btn-primary m-2"
+						className="btn btn-primary m-2"
 						key={a}
 						value={a}
+						onClick={handleClickLetter}
+						disabled={guessed.has(a)}
 					>
 						{a}
 					</button>
 				))}
+			</div>
+
+			<div>
+				<button className="btn btn-warning my-5" onClick={resetGame}>
+					Reset
+				</button>
 			</div>
 		</div>
 	);
