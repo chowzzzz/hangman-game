fc5265d3260fc04c07c8380ae01a3d3984b349b8
dearvae <lovevae7@gmail.com>
2022-05-29 20:53:47 +0800

    add fail screen

--- src/components/Fail.js
+++ src/components/Fail.js
@@ -2,18 +2,23 @@ import React from "react";
 
 const Fail = () => {
     return (
-        <div class='container'>
-            You Died!
-            <div class='tear'></div>
-            <div class='tear2'></div>
-            <div class='face'>
-                <div class='eyebrow'>︶</div>
-                <div class='eyebrow'>︶</div>
-                <div class='eye'></div>
-                <div class='eye'></div>
-                <div class='mouth'></div>
+        <>
+            <div className="fail-text">
+                You Died!
             </div>
-        </div>
+            <div class='container'>
+
+                <div class='tear'></div>
+                <div class='tear2'></div>
+                <div class='face'>
+                    <div class='eyebrow'>︶</div>
+                    <div class='eyebrow'>︶</div>
+                    <div class='eye'></div>
+                    <div class='eye'></div>
+                    <div class='mouth'></div>
+                </div>
+            </div>
+        </>
     );
 };
 
